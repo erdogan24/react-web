269b6e4f2b7166f0a39286c11cbea1f31e0bd1c8
Erdoğan <77540591+erdogan24@users.noreply.github.com>
2022-09-22 03:37:17 +0300

    September 22, 3.37 AM

--- src/pages/UserSignupPage.js
+++ src/pages/UserSignupPage.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { signup } from '../api/apiCalls';
+import Input from '../components/Input';
 
 class UserSignupPage extends React.Component {
-
 
     state = {
         username: null,
@@ -15,7 +15,7 @@ class UserSignupPage extends React.Component {
     onChange = event => {
 
         const { name, value } = event.target;
-        const errors = { ... this.state.errors }
+        const errors = { ...this.state.errors }
         errors[name] = undefined
         this.setState({
             [name]: value,
@@ -24,7 +24,6 @@ class UserSignupPage extends React.Component {
     };
     onClickSignup = async event => {
         event.preventDefault();
-
         const { username, nickName, password } = this.state;
         const body = {
             username,
@@ -32,63 +31,39 @@ class UserSignupPage extends React.Component {
             password
         };
         this.setState({ pendingApiCall: true });
-
         try {
             const response = await signup(body);
         } catch (error) {
-            if(error.response.data.validationErrors){
+            if (error.response.data.validationErrors) {
                 this.setState({ errors: error.response.data.validationErrors });
             }
-            
         }
         this.setState({ pendingApiCall: false });
-
     };
     render() {
         const { pendingApiCall, errors } = this.state;
-        const { username } = errors;
+        const { username, nickName } = errors;
+
         return (
             <div className="cantainer">
                 <form>
-
                     <h1 className="text-center">Sign Up</h1>
-                    <div className="form-group">
-                        <label>Username</label>
-                        <input className={username ? "form-control is-invalid" : 'form-control'} name="username"
-                            onChange={this.onChange}
-                        />
-                        <div className="invalid-feedback">
-                            {username}
-                        </div>
-                    </div>
+                    <Input name="username" label="Username" error={username} onChange={this.onChange} />
+                    <Input name="nickName" label="Nick Name" error={nickName} onChange={this.onChange} />
 
-                    <div className="form-group">
-                        <label>Nick name</label>
-                        <input className="form-control" name="nickName"
-                            onChange={this.onChange} />
-                    </div>
-
-                    <div className="form-group">
-                        <label>Password </label>
+                    <div className="form-group"><label>Password </label>
                         <input className="form-control" name="password" type="password"
                             onChange={this.onChange} />
                     </div>
-
-                    <div className="form-group">
-                        <label>Password Repeat </label>
+                    <div className="form-group"><label>Password Repeat </label>
                         <input className="form-control" name="passwordRepeat" type="password"
                             onChange={this.onChange} />
                     </div>
-                    <div className="text-center">
-                        <br></br>
-                        <button className="btn btn-primary"
-                            onClick={this.onClickSignup}
-                            disabled={pendingApiCall}
-                        >
+                    <div className="text-center"><br></br>
+                        <button className="btn btn-primary" onClick={this.onClickSignup} disabled={pendingApiCall} >
 
                             {pendingApiCall && <span className="spinner-border spinner-border-sm" ></span>} Sign Up
                         </button>
-
                     </div>
                 </form>
             </div>
